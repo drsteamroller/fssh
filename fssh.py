7043138f7781f31fd787ef8c8f09f5290b25d749
Andrew McConnell <amcconnell886@gmail.com>
2023-12-17 12:50:20 -0500

    shell program

--- fssh.py
+++ fssh.py
@@ -1,1 +1,29 @@
 # Author: Andrew McConnell
+
+import argparse
+import paramiko
+from getpass import getpass
+import re
+
+class Grep():
+
+    def __init__(self):
+        self.pattern = ""
+        self.options = []
+        self.subject = ""
+
+    def search(self):
+        pass
+
+    def run(self, options: list, PATTERN: str, subject: str):
+
+        self.options = options
+
+        if '-e' in options:
+            self.pattern = re.compile(PATTERN)
+        else:
+            self.pattern = PATTERN
+        self.subject = subject
+
+
+        
